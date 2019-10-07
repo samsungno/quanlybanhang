c17d2903ef5bbdb7a32ccc18d20be3c398dc341b
samsungno <56260737+samsungno@users.noreply.github.com>
2019-10-07 20:55:03 +0700

    tao form dang nhap

---
+++ QuanLyBanHang/QuanLyBanHang/Frmdangnhap.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QuanLyBanHang
+{
+    partial class Frmdangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttaikhoan = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.btndangnhap = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuanLyBanHang.Properties.Resources._800px_Kudat_Sabah_Fuk_Tek_Kung_Temple_02;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(69, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quan Ly Ban hang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nhập tên tài khoản";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập mật khẩu";
+            // 
+            // txttaikhoan
+            // 
+            this.txttaikhoan.Location = new System.Drawing.Point(116, 253);
+            this.txttaikhoan.Name = "txttaikhoan";
+            this.txttaikhoan.Size = new System.Drawing.Size(160, 20);
+            this.txttaikhoan.TabIndex = 3;
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(116, 289);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(160, 20);
+            this.txtmatkhau.TabIndex = 3;
+            this.txtmatkhau.UseSystemPasswordChar = true;
+            // 
+            // btndangnhap
+            // 
+            this.btndangnhap.Location = new System.Drawing.Point(116, 328);
+            this.btndangnhap.Name = "btndangnhap";
+            this.btndangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btndangnhap.TabIndex = 4;
+            this.btndangnhap.Text = "Đăng nhập";
+            this.btndangnhap.UseVisualStyleBackColor = true;
+            this.btndangnhap.Click += new System.EventHandler(this.btndangnhap_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnthoat.Location = new System.Drawing.Point(201, 328);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.TabIndex = 4;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            // 
+            // Frmdangnhap
+            // 
+            this.AcceptButton = this.btndangnhap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnthoat;
+            this.ClientSize = new System.Drawing.Size(306, 372);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btndangnhap);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.txttaikhoan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frmdangnhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dang nhap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttaikhoan;
+        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.Button btndangnhap;
+        private System.Windows.Forms.Button btnthoat;
+    }
+}
+
---
+++ QuanLyBanHang/QuanLyBanHang/Frmdangnhap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang
+{
+    public partial class Frmdangnhap : Form
+    {
+        public Frmdangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndangnhap_Click(object sender, EventArgs e)
+        {
+            if(txttaikhoan.Text != "admin" && txtmatkhau.Text !="123456"){
+                MessageBox.Show("Đăng nhập không thành công! Vui lòng kiểm tra lại tài khoản và mật khẩu.");
+            }
+            else
+            {
+                MessageBox.Show("Bạn đã đăng nhập Thành công!!!");
+                Frmmain frm = new Frmmain();
+                frm.Show();
+                this.Hide();
+
+               
+            }
+        }
+    }
+}
